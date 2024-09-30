7d51e84fabe644f9b921b4af76f8b6dfd0f0ec29
Duvier <duvier.tavera01@unicatolica.edu.co>
2024-09-30 10:43:15 -0500

    Se modifico la barra de busqueda

--- script.js
+++ script.js
@@ -13,5 +13,35 @@ document.getElementById('registro-link').addEventListener('click', function() {
         window.location.href = 'login.html'; 
     });
 
+    document.querySelector('.search-btn').addEventListener('click', function() {
+        const searchValue = document.querySelector('.search').value.toLowerCase();
+        
+        const products = document.querySelectorAll('.producto');
+        let found = false; 
+
+        products.forEach(function(product) {
+            const productName = product.querySelector('.producto-nombre').textContent.toLowerCase();
+            
+            if (productName.includes(searchValue)) {
+                product.style.display = 'block';  
+                found = true; 
+            } else {
+                product.style.display = 'none'; 
+            }
+        });
+
+        if (!found) {
+            document.getElementById('no-result').style.display = 'block';
+        } else {
+            document.getElementById('no-result').style.display = 'none';
+        }
+    });
+
+
+    document.querySelector('.search').addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            document.querySelector('.search-btn').click();
+        }
+    });
 
     
